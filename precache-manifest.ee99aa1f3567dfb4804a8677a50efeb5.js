e9f7fb53932fe9097247485fb040dea2df9a9980
andrayantelo <andrayantelo@gmail.com>
2020-03-13 16:55:57 -0700

    Updates

---
+++ precache-manifest.ee99aa1f3567dfb4804a8677a50efeb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f2a53444f11d3d01d51a1aa5303abef",
+    "url": "andrayantelo.github.io/react-calculator/index.html"
+  },
+  {
+    "revision": "d40ba86bc837d61e0cac",
+    "url": "andrayantelo.github.io/react-calculator/static/css/main.772d00b4.chunk.css"
+  },
+  {
+    "revision": "f6692349757ee62baf51",
+    "url": "andrayantelo.github.io/react-calculator/static/js/2.7526108b.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "andrayantelo.github.io/react-calculator/static/js/2.7526108b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d40ba86bc837d61e0cac",
+    "url": "andrayantelo.github.io/react-calculator/static/js/main.59ee969d.chunk.js"
+  },
+  {
+    "revision": "9e99bfbc94315a5ccede",
+    "url": "andrayantelo.github.io/react-calculator/static/js/runtime-main.1037c1f6.js"
+  }
+]);
